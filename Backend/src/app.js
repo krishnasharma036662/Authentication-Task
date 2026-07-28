5013e8d37d88dadf1bc4d3ec988dbdd6c3395c43
ks <ks7163464@gmail.com>
2026-07-28 18:45:14 +0530

    Allow Vercel frontend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ import authRouter from "./routes/auth.route.js";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://authentication-task-krishna-sharma-s-projects1.vercel.app",
     credentials: true
 }));
 
